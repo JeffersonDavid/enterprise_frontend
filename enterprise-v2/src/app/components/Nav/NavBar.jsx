ae2b3971b63dbb01983e5d394e7be6bad523c8a9
JeffersonDavid <jeffersondvid@hotmail.com>
2023-08-10 13:49:49 +0200

    add menu fixe

--- enterprise-v2/src/app/components/Nav/NavBar.jsx
+++ enterprise-v2/src/app/components/Nav/NavBar.jsx
@@ -3,13 +3,13 @@
 import React, { useState,useEffect,useRef } from 'react'; 
 
 import Image from "next/image"
-import Logo from "../../../../public/images/logo.png"
 import Link from "next/link"
+
+import Logo from "../../../../public/images/logo.png"
 import PitaIcon from "../../../../public/images/pan-de-pita.png"
 import ComunincationIcon from "../../../../public/images/com.png"
 import BoxIcon from "../../../../public/images/box.png"
 import CompanyIcon from "../../../../public/images/panadero.png"
-
 import Close from "../../../../public/images/close.png"
 
 
@@ -115,7 +115,7 @@ return (
         {
             pages.map((element,index) => {
                return( 
-                        <div className="link-container" key={index}>
+                        <div className="link-container link_responsive hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700" key={index}>
                             <li className="link-navi">
                                   <Link href={element.source} className="block py-2 pl-3 pr-4 text-zinc-950 rounded md:bg-transparent md:p-0" onClick={() => setmobile_menu_state(false)} >{element.label}</Link>
                             </li>
@@ -127,8 +127,7 @@ return (
         }
 
       <div className="linkcontainer drsignal">
-            <button onClick={menucontroller} id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" className="flex items-center justify-between w-full py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto drsignal">
-            <Image src={BoxIcon}  className='drsignal' width={20} height={20} alt="Com"/> Pedidos <svg className="w-5 h-5 ml-1 drsignal" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"><path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd"></path></svg></button>
+            <button onClick={menucontroller} id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" className="flex items-center justify-between w-full py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto drsignal"> Pedidos <svg className="w-5 h-5 ml-1 drsignal" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"><path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd"></path></svg> <Image src={BoxIcon}  className='drsignal icon_menu' width={20} height={20} alt="Com"/> </button>
                 <div ref={dropRef} id="dropdownNavbar" className=" drsignal z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44">
                           <ul className="py-2 text-sm text-gray-700 drsignal" aria-labelledby="dropdownLargeButton">
                             <li key={1} className='drsignal'>

ba88581171be0a55a2801661f68384c0bde20b97
Jefferson David Coba <e-jdcb@gft.com>
2023-09-02 14:03:56 +0200

    imporves

--- enterprise-v2/src/app/components/Product/Product.jsx
+++ enterprise-v2/src/app/components/Product/Product.jsx
@@ -2,15 +2,8 @@
 'use client'
 
 import React from 'react';
-
 import Image from 'next/image';
-
-import productData from '../../data/data';
-
 import dataset from './data'
-
-import ComunincationIcon from "../../../../public/images/com.png"
-
 import { useState, useEffect } from 'react';
 
 
@@ -36,19 +29,28 @@ return (
 
 <div className='secure-container flex justify-center items-center'>
 
-  <div className="flex product_desc_width_he border border-gray-300 rounded p-2 w-[70%] h-[60%]">
+  <div className="flex product_desc_width_he border border-gray-300 rounded p-2 w-[70%] h-[65%]">
     <div className="w-1/10 p-1 border border-gray-100 rounded m-auto mr-3">
       <div className='p-1'>
-        <Image src={selectedData.features.generic.img} alt="Imagen 1" width={120} height={120} className='border-b border-gray-300 rounded min_pr_image' />
+        <Image src={selectedData.img} alt="Imagen 1" width={120} height={120} className='border-b border-gray-300 rounded min_pr_image' />
         <br></br>
-        <Image src={selectedData.features.generic.img} alt="Imagen 2" width={120} height={120} className='min_pr_image' />
+        <Image src={selectedData.img} alt="Imagen 2" width={120} height={120} className='min_pr_image' />
         <br></br>
-        <Image src={selectedData.features.generic.img} alt="Imagen 3" width={120} height={120} className='min_pr_image' />
+        <Image src={selectedData.img} alt="Imagen 3" width={120} height={120} className='min_pr_image' />
       </div>
     </div>
 
     <div className="col2img w-3/10 p-1 flex flex-col justify-between m-auto h-full mr-2">
-      <Image src={selectedData.features.generic.img} alt="Imagen 4" width={500} height={500} className='border border-gray-200 rounded med_pr_image object-cover' />
+      <Image src={selectedData.img} alt="Imagen 4" width={500} height={500} className='border border-gray-200 rounded med_pr_image object-cover' />
+      <button className="btnbcolor inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300" type="button" onClick={e => SendData(e)}>
+        <p>Pídelo ahora</p>
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="ml-auto">
+          <circle cx="9" cy="21" r="1"/>
+          <circle cx="20" cy="21" r="1"/>
+          <path d="M21 15a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h8l2 5h5a2 2 0 0 1 2 2z"/>
+        </svg>
+      </button>
+
     </div>
 
     <div className="w-[80%] md:w-[90%] p-2 border border-gray-100 rounded descbox">
@@ -57,27 +59,54 @@ return (
 
       
           <ul className="max-w-md divide-y divide-gray-200 dark:divide-gray-700">
+                
+                 {
+                      selectedData.features.generic.map((item, index) =>
+                      (
+                          <li  key={index} className="">
+                            <div className="flex items-center space-x-1 mt-1 mb-1">
+                              <div className="flex-1 min-w-0">
+                                  <p className="text-xs font-medium text-gray-900 truncate dark:text-white">
+                                    {item.key}
+                                  </p>
+                                  <p className="text-sm text-gray-500 truncate dark:text-gray-400">
+                                  {item.value}
+                                  </p>
+                              </div>
+                              <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">  
+                              </div>
+                            </div>
+                        </li>
+                      ))
 
-            {
-                  selectedData.features.bag.map((item, index) =>
-                   (
-                      <li  key={index} className="">
-                        <div className="flex items-center space-x-1 mt-1 mb-1">
-                          <div className="flex-1 min-w-0">
-                              <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
-                                {item.key}
-                              </p>
-                              <p className="text-sm text-gray-500 truncate dark:text-gray-400">
-                              {item.value}
-                              </p>
-                          </div>
-                          <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white"> 
-                          ?
-                          </div>
-                        </div>
-                    </li>
-                  ))
-            }
+                  }
+
+                  {
+                      selectedData.features.bag.map((item, index) =>
+                      (
+                          <li  key={index} className="">
+                            <div className="flex items-center space-x-1 mt-1 mb-1">
+                              <div className="flex-1 min-w-0">
+                                  <p className="text-xs font-medium text-gray-900 truncate dark:text-white">
+                                    {item.key}
+                                  </p>
+                                  <p className="text-sm text-gray-500 truncate dark:text-gray-400">
+                                  {item.value}
+                                  </p>
+                              </div>
+                              <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white"> 
+                              {
+                                item.price ?
+                                <span class="bg-green-100 text-green-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300">{item.total} </span> 
+                                  :
+                                  ''
+                              }
+                              </div>
+                            </div>
+                        </li>
+                      ))
+                }
+         
              
           </ul>
 
--- enterprise-v2/src/app/components/Product/data.js
+++ enterprise-v2/src/app/components/Product/data.js
@@ -10,33 +10,45 @@ export default function dataset(){
 
 const product_data = {
 
-    integral : {
-
-        
-
+    integral : { 
         features : 
         {
-            bag : [
-                
-
-                { key : 'Bolsas en caja', value : 10 },
-                { key : 'Duracion', value : 10 },
-                { key : 'Almacenamiento', value : 10 },
-                { key : 'Dimension bolsa', value : 10 },
-                { key : 'Dimension caja ', value : 10 },
-                { key : 'Pitas en bolsa ', value : 10 },
-            ],
-            generic:{
-                long_desc:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla convallis libero ut ultrices. Sed dapibus sapien at libero vehicula, in venenatis ligula suscipit.',
-                img:pitaint_img
-            }
-           
-        }
-        
-
-
+                bag : [
+                    { key : ' Cantidad (caja) ', value : `Cada caja contiene ${30} bolsas en cada caja.` },
+                    { key : ' Cantidad (bolsa) ', value : `Cada bolsa contiene ${6} pitas en cada bolsa.` },
+                    { key : ' Medidas ( pita ) ', value : `Cada pita mide 20 cm.` },
+                    { key : ' Precio ( bolsa ) ', value : `El precio de cada bolsa es de `, price: true , total: '1€'},
+                    { key : ' Precio ( bolsa ) ', value : `El precio de cada caja es de `, price: true , total: '26 €'},
+                ],
+                generic:[
+                    { key : ' Duracion ', value : 'En temperatura ambiente: 15 días. Congelado hasta 3 meses.' },
+                    { key : ' Almacenamiento ( pita ) ', value : `Es apto para congelar.` },
+                ]
+            
+        },
+        img:pitaint_img
 
     },
+
+    blanco : { 
+        features : 
+        {
+                bag : [
+                    { key : ' Cantidad (caja) ', value : `Cada caja contiene ${30} bolsas en cada caja.` },
+                    { key : ' Cantidad (bolsa) ', value : `Cada bolsa contiene ${6} pitas en cada bolsa.` },
+                    { key : ' Medidas ( pita ) ', value : `Cada pita mide 20 cm.` },
+                    { key : ' Precio ( bolsa ) ', value : `El precio de cada bolsa es de `, price: true , total: '1€'},
+                    { key : ' Precio ( bolsa ) ', value : `El precio de cada caja es de `, price: true , total: '26 €'},
+                ],
+                generic:[
+                    { key : ' Duracion ', value : 'En temperatura ambiente: 15 días. Congelado hasta 3 meses.' },
+                    { key : ' Almacenamiento ( pita ) ', value : `Es apto para congelar.` },
+                ]
+            
+        },
+
+        img:pitablanco_img
+    }
    
 }
 

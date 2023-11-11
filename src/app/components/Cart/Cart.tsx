e3565dfc6b87a944786658ff9a8abaf92afb0fa4
JeffersonDavid <jeffersondvid@hotmail.com>
2023-11-11 22:31:21 +0000

    add form

--- src/app/components/Cart/Cart.tsx
+++ src/app/components/Cart/Cart.tsx
@@ -1,5 +1,6 @@
 'use client'
 import React from "react"
+import { ReactNode } from 'react';
 import { CartContract } from './Contracts'
 import { securevalidation, checkLocalStorage, mustShowCartDetails } from './CartUtils'
 import Link from "next/link";
@@ -8,11 +9,10 @@ import { useEffect,  useState } from 'react';
 
 export default function CartComponent( props: CartContract) {
 
+    let html: ReactNode  = null;
     const persistance = checkLocalStorage(props);
     const validation = securevalidation( props );
-
     const [cartsProps, setcartsProps] = useState( JSON.parse(localStorage.getItem('CartProps')));
-
     const [showPage, setShowPage] = useState(false);
 
     useEffect(() => {
@@ -20,7 +20,7 @@ export default function CartComponent( props: CartContract) {
       }, [cartsProps]); 
 
 
-    let html = null;
+    
 
     switch (props.data.type_component) {
 
@@ -35,9 +35,49 @@ export default function CartComponent( props: CartContract) {
             if(showPage){
 
                 html =  <div className="h-screen w-screen flex items-center justify-center">
-                            <div className="bg-white rounded-lg shadow-lg p-4">
-                                        HAY PRODUCTOS PARA MOSTRAR
-                            </div>
+                            
+
+                                    <form  className="mwcustom bg-white w-full shadow-md rounded px-8 pt-2 pb-2 mb-1">
+
+                                        <div className="mb-4">
+                                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+                                                Nombre y Apellidos
+                                            </label>
+                                            <input id="username" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"placeholder="introduce tu Nombre completo..." />
+                                        </div>
+
+                                        <div className="mb-4">
+                                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+                                                Email
+                                            </label>
+                                            <input id="email" type="email" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="intruduce tu email..."/>
+                                        </div>
+
+                                        <div className="mb-4">
+
+                                                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="products">Productos añadidos : </label>
+
+                                                <div>
+                                                        <label className="ml-2 text-gray-700 text-sm font-bold w-full" htmlFor="products"> - Pita blanco </label>
+
+                                                        <div className="flex">
+
+                                                            <label className="ml-4 text-gray-700 text-sm font-bold w-full" htmlFor="products">
+                                                                <span className="text-red-500">  * </span> Seleccione una cantidad: 
+                                                            </label>
+
+                                                            <div className="flex">
+                                                                    <input id="products" type="number" className="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder=""/>
+                                                            </div>
+                                                    </div>
+
+                                                </div>
+                                          
+                                        </div>
+
+                                                
+                                    </form>
+                           
                          </div>
 
             }else{
@@ -52,6 +92,7 @@ export default function CartComponent( props: CartContract) {
 
             }
 
+        break
         case 'simpleLink':
 
             if(showPage){
--- src/app/my-orders/page.jsx
+++ src/app/my-orders/page.jsx
@@ -7,7 +7,7 @@ export default function MyOrdersPage() {
 
 return(  
          <div className='secure-container flex justify-center items-center' > 
-            <CartComponent data={{ type_component: 'page', products_added: [] }} />  
+             <CartComponent data={{ type_component: 'page', products_added: [] }} />  
          </div>
     )
 }
